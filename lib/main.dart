870dddcd40a2c0e14deee3a9330682b316c07abc
KarelinMerei <karelinmerei@gmail.com>
2024-05-27 16:40:26 +0500

    another_variant

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class _MyAppState extends State<MyApp> {
 
   Future<void> _initializeWebview() async {
     await _webviewScreenshot.initialize();
+
     String htmlContent = '''
 <!DOCTYPE html>
 <html lang="en">
@@ -196,11 +197,177 @@ class _MyAppState extends State<MyApp> {
 </body>
 </html>
     ''';
+
     await _webviewScreenshot.loadHtmlContent(htmlContent);
   }
 
   Future<void> _takeScreenshot() async {
-    final screenshot = await _webviewScreenshot.captureScreenshot();
+    final screenshot = await renderHtmlToImage('''<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dynamic Table Example</title>
+    <style>
+        table {
+            width: 100%;
+        }
+        #paymentsTable th, #paymentsTable td:first-child {
+            text-align: center;
+            font-size: 2em;
+        }
+        #paymentsTable td:nth-child(2) {
+            font-size: 2em;
+        }
+        #paymentsTable td:nth-child(3) {
+            text-align: right;
+            font-size: 2em;
+        }
+        #tableInfo td:nth-child(1) {
+            text-align: left;
+            font-size: 2em;
+        }
+        #tableInfo td:nth-child(2) {
+            text-align: right;
+            font-size: 2em;
+        }
+        #version, #lastSync {
+            text-align: center;
+            font-size: 1.5em;
+            font-weight: bold;
+            margin-top: 5px;
+            margin-bottom: 10px;
+        }
+        #paymentsTable ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+        }
+        #foo {
+            min-height: auto;
+        }
+        h2 {
+            text-align: center;
+            font-size: 2.5em;
+            margin-bottom: 10px;
+        }
+        #idout, #tableName, #subdivision, #note {
+            text-align: center;
+            font-size: 2em;
+            margin-top: 5px;
+            margin-bottom: 10px;
+        }
+        .modifier-list {
+            padding-left: 10px;
+        }
+        .parent {
+            text-align: center;
+        }
+        .child {
+            display: inline-block;
+            vertical-align: middle;
+        }
+    </style>
+</head>
+<body>
+    <div id="foo">
+        <div id="receipt-image">
+            <h2 id="recieptTitle"></h2>
+            <div id="subdivision"></div>
+            <div class="parent">
+                <div class="child" id="idout"></div>
+                <div class="child" id="tableName"></div>
+            </div>
+            <div id="note"></div>
+            <table id="tableInfo" border="0">
+                <tbody>
+                    <tr>
+                        <td id="printedDateTitle"></td>
+                        <td id="printedDate"></td>
+                    </tr>
+                </tbody>
+            </table>
+            <br>
+            <table id="paymentsTable" border="1">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th id="nameTableRow"></th>
+                        <th id="priceTableRow"></th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+        <br>
+        <div id="version"></div>
+        <div id="lastSync"></div>
+        <script>
+            var prindata = [];
+            if (prindata.length == 0) {
+                prindata = [
+                    {
+                        "recieptTitle":"Kitchen receipt",
+                        "nameTableRow":"value",
+                        "priceTableRow":"price",
+                        "note":"dine in",
+                        "idout":"#9",
+                        "subdivision":"desert",
+                        "version":"0.1.5-DEBUG",
+                        "lastSync":"2024-04-24T09:39:42.085",
+                        "tableName":"table  8",
+                        "items":[
+                            {
+                                "#":"1",
+                                "name":"1x pepsi ",
+                                "modifiers":[],
+                                "price":"900.0",
+                                "printPriority":"FIRE"
+                            }
+                        ]
+                    }
+                ];
+            }
+            function populateTable(data) {
+                var table = document.getElementById('paymentsTable').getElementsByTagName('tbody')[0];
+                data.forEach(function(item) {
+                    var newRow = table.insertRow(table.rows.length);
+                    var cell1 = newRow.insertCell(0);
+                    var cell2 = newRow.insertCell(1);
+                    var cell3 = newRow.insertCell(2);
+                    cell1.innerHTML = item["#"];
+                    cell2.innerHTML = '<strong>' + item.printPriority + '</strong> ' + item.name + renderModifiers(item.modifiers, 'name');
+                    cell3.innerHTML = item.price + renderModifiers(item.modifiers, 'price');
+                });
+            }
+            function renderModifiers(modifiers, field) {
+                if (!modifiers || modifiers.length === 0) {
+                    return '';
+                }
+                var result = '<ul class="modifier-list">';
+                modifiers.forEach(function(modifier) {
+                    result += '<li>' + '&nbsp;&nbsp;' + modifier[field] + '</li>';
+                });
+                result += '</ul>';
+                return result;
+            }
+            populateTable(prindata[0]['items']);
+            document.getElementById('recieptTitle').innerHTML = prindata[0]['recieptTitle'];
+            document.getElementById('nameTableRow').innerHTML = prindata[0]['nameTableRow'];
+            document.getElementById('priceTableRow').innerHTML = prindata[0]['priceTableRow'];
+            document.getElementById('idout').innerHTML = prindata[0]['idout'];
+            document.getElementById('tableName').innerHTML = prindata[0]['tableName'];
+            document.getElementById('note').innerHTML = prindata[0]['note'][0].toUpperCase() + prindata[0]['note'].substring(1);
+            document.getElementById('version').innerHTML = 'PalomaPOS v' + prindata[0]['version'];
+            document.getElementById('lastSync').innerHTML = prindata[0]['lastSync'];
+            document.getElementById('subdivision').innerHTML = prindata[0]['subdivision'];
+            document.getElementById('printedDateTitle').innerHTML = prindata[0]['printedDateTitle'];
+            document.getElementById('printedDate').innerHTML = prindata[0]['printedDate'] + '&nbsp;' + prindata[0]['printedTime'];
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.5/dist/html2canvas.min.js" defer></script>
+        <hr />
+        <hr />
+    </div>
+</body>''');
     setState(() {
       _screenshot = screenshot;
     });
--- lib/webview.dart
+++ lib/webview.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:screenshot/screenshot.dart';
@@ -30,4 +31,56 @@ class WebviewScreenshot {
 
   WebviewController get controller => _controller;
   ScreenshotController get screenshotController => _screenshotController;
+}
+
+Future<Uint8List?> renderHtmlToImage(String htmlContent) async {
+  final WebviewController controller = WebviewController();
+  await controller.initialize();
+  final completer = Completer<Uint8List?>();
+
+  controller.webMessage.listen((message) {
+    if (message.message.containsKey('screenshot')) {
+      final base64image = message.message['screenshot'];
+      final bytes = base64Decode(base64image.split(',').last);
+      completer.complete(bytes);
+    }
+  });
+
+  await controller.loadStringContent(
+    '''
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Screenshot</title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
+        <script>
+          function captureScreenshot() {
+            html2canvas(document.body).then(canvas => {
+              const base64image = canvas.toDataURL('image/png');
+              window.chrome.webview.postMessage({screenshot: base64image});
+            }).catch(err => {
+              console.error('Error capturing screenshot:', err);
+              window.chrome.webview.postMessage({error: err.toString()});
+            });
+          }
+
+          document.addEventListener('DOMContentLoaded', (event) => {
+            captureScreenshot();
+          });
+        </script>
+      </head>
+      <body>
+        $htmlContent
+      </body>
+      </html>
+    ''',
+  );
+
+  controller.webMessage.listen((message) {
+    if (message.message.containsKey('error')) {
+      completer.completeError(Exception(message.message['error']));
+    }
+  });
+
+  return await completer.future;
 }
