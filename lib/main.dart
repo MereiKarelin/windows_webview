32eb263c0c994374753735e8d09da22e1534f125
KarelinMerei <karelinmerei@gmail.com>
2024-05-27 15:13:56 +0500

    with windows webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,66 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:universal_io/io.dart';
-import 'package:webview_universal/webview_desktop/webview_desktop.dart' as webview_desktop;
-import 'package:flutter/services.dart' show rootBundle;
+import 'package:webview_windows/webview_windows.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await WebviewController.initializeEnvironment();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final WebviewController _controller = WebviewController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WebViewExample(),
+      home: Scaffold(
+        appBar: AppBar(title: Text('WebView Example')),
+        body: FutureBuilder<void>(
+          future: _controller.initialize(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Webview(
+                _controller,
+                width: 800,
+                height: 600,
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _controller.loadUrl('https://flutter.dev');
+          },
+          child: Icon(Icons.web),
+        ),
+      ),
     );
   }
 }
 
-class WebViewExample extends StatefulWidget {
-  @override
-  _WebViewExampleState createState() => _WebViewExampleState();
-}
-
-class _WebViewExampleState extends State<WebViewExample> {
-  late webview_desktop.Webview _webview;
-  bool _isWebViewInitialized = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _initializeWebView();
-  }
-
-  Future<void> _initializeWebView() async {
-    bool isWebviewAvailable = await webview_desktop.WebviewWindow.isWebviewAvailable();
-    if (isWebviewAvailable) {
-      _webview = await webview_desktop.WebviewWindow.create(
-        configuration: webview_desktop.CreateConfiguration(
-          titleBarTopPadding: Platform.isMacOS ? 20 : 0,
-        ),
-      );
-      setState(() {
-        _isWebViewInitialized = true;
-      });
-
-      _webview.setBrightness(Brightness.dark);
-
-      // Load the HTML content
-      String htmlContent = await rootBundle.loadString('assets/screen_capture.html');
-      _webview.launch(Uri.dataFromString(
-        htmlContent,
-        mimeType: 'text/html',
-        encoding: Encoding.getByName('utf-8'),
-      ).toString());
-    }
-  }
+class WebviewPage extends StatelessWidget {
+  final WebviewController controller = WebviewController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('WebView Screen Capture'),
-      ),
-      body: _isWebViewInitialized ? Container() : const Center(child: CircularProgressIndicator()),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_isWebViewInitialized) {
-            _captureScreen();
+      appBar: AppBar(title: Text('WebView Page')),
+      body: FutureBuilder<void>(
+        future: controller.initialize(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Webview(
+              controller,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
           }
         },
-        child: const Icon(Icons.camera),
       ),
     );
-  }
-
-  Future<void> _captureScreen() async {
-    try {
-      // JavaScript code to capture the screen
-      final String jsCode = '''
-        (async function() {
-          try {
-            const stream = await navigator.mediaDevices.getDisplayMedia({ video: true });
-            const video = document.createElement('video');
-            video.srcObject = stream;
-            video.play();
-
-            video.onloadedmetadata = async () => {
-              const canvas = document.createElement('canvas');
-              canvas.width = video.videoWidth;
-              canvas.height = video.videoHeight;
-              const context = canvas.getContext('2d');
-              context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-              const dataURL = canvas.toDataURL('image/png');
-              window.screenshotHandler.postMessage(dataURL);
-
-              video.srcObject.getTracks().forEach(track => track.stop());
-            };
-          } catch (err) {
-            console.error('Error: ' + err);
-          }
-        })();
-      ''';
-
-      // Execute JavaScript code in the WebView
-      await _webview.evaluateJavaScript(jsCode);
-    } catch (e) {
-      print('Error capturing screen: $e');
-    }
   }
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <webview_universal/webview_universal_plugin.h>
+#include <webview_windows/webview_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   WebviewUniversalPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("WebviewUniversalPlugin"));
+  WebviewWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WebviewWindowsPlugin"));
 }
